6562a766f6d8498ba0e9ccc6aa97f60fa747b1fb
Md Manowar Alam <mdmanowar077@gmail.com>
2026-06-19 18:28:56 +0530

    up

--- MistralChatBot.py
+++ MistralChatBot.py
@@ -7,10 +7,13 @@ llm = ChatMistralAI(
     model="mistral-small-2506",
     temperature=0.9
 )
+messages=[]
 while True:
     prompt=input("Enter your question: ")
+    messages.append(prompt)
     if prompt=="0":
         break
-    response = llm.invoke(prompt)
+    response = llm.invoke(messages)
+    messages.append(response.content)
 
-    print("Bot :hello",response.content)
+    print("Bot :hello",response.content) 
